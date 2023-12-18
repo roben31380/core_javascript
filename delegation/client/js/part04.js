befff6855f1931d06e165f5028f1167545cb60a0
roben31380 <kinalee0430@gmail.com>
2023-12-18 13:43:11 +0900

    dom

--- delegation/client/js/part04.js
+++ delegation/client/js/part04.js
@@ -1,3 +1,18 @@
+const content = document.body.querySelector('.contents');
 
+function handleClick(e) {
+  e.preventDefault;
+  let target = e.target;
 
+  while (target.getAttribute('data-name')) {
+    target = target.parentElement;
 
+    if (target.tagName === 'BODY') {
+      target = null;
+      return;
+    }
+  }
+  console.log(target);
+}
+
+content.addEventListener('click', handleClick);
